05231e88d4f951f1470cab5b4a49220b03e5fb75
P. Tomas <ptomas@astr-ptomas.edcs.fr>
2021-08-23 11:38:43 +0200

    update

--- update_vim.sh
+++ update_vim.sh
@@ -4,9 +4,13 @@ DIR=/tmp/vim_repo_clone
 
 [ -d $DIR ] && rm -rf $DIR
 mkdir -p $DIR && cd $DIR
+git clone https://github.com/preservim/nerdtree.git
+rm -rf ~/.vim/*
+cp -r nerdtree/* ~/.vim
 git clone https://github.com/vim/vim.git
 cd vim/src
 ./configure && make && sudo make install
+
 cd $DIR && rm -rf $DIR
 cd ~
 vim --version
