b761f06c26304cde0c6200a94c23c98660984008
P. Tomas <ptomas@astr-ptomas.edcs.fr>
2021-10-01 14:43:38 +0200

    [fixing - .bash_aliases] ls and ll functions improved with grep usage instead of tail

--- autostart/scripts/autostart_redshift.sh
+++ autostart/scripts/autostart_redshift.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 [ $(which glxinfo | wc -l) -eq 0 ] && exit 1
-[ $(glxinfo | grep -E -i "Device" | wc -l) -eq 0 ] \
+[ $(glxinfo | grep -E -i "Device" | wc -l) -eq 0 ] && exit 1
 [ $(which redshift | wc -l) -eq 0 ] && exit 1
 
 redshift -x > /dev/null
