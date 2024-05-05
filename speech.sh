cf5965b291c6947ebb2dbcf9218a8fb79804e9e8
tiawl <pabtomas@protonmail.com>
2024-05-06 00:40:44 +0200

    [update] add indirection into /run/default.conf

--- speech.sh
+++ speech.sh
@@ -7,4 +7,5 @@ docker compose -f ./components/compose.yaml --env-file ./.env build
 docker compose build
 docker compose create
 docker compose start
+docker image prune --all -f
 docker attach jumper
