b99acc129331c389ba3f522fb12d74b36c77b47a
tiawl <pabtomas@protonmail.com>
2024-05-06 17:25:06 +0200

    [update] USE_PROXY + show proxy logs

--- speech.sh
+++ speech.sh
@@ -1,11 +1,13 @@
 #! /usr/bin/env bash
 
 set -e
+source .env
 docker compose down || :
 docker volume rm shared-ssh || :
 docker compose -f ./components/compose.yaml --env-file ./.env build
 docker compose build
 docker compose create
 docker compose start
-docker image prune --all -f
+docker logs proxy 2> /dev/null
+docker image prune --all -f > /dev/null
 docker attach jumper
