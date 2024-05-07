a702f6751d7cfe24284a715805cff827c46b055e
tiawl <pabtomas@protonmail.com>
2024-05-08 00:02:13 +0200

    [update] jumper: cache autocompletion with CRON

---
+++ README.md
@@ -0,0 +1,5 @@
+# my wheel fleet
+
+Work in progress ...
+
+This README is coming soon
--- wget_me.sh
+++ wget_me.sh
@@ -3,11 +3,11 @@
 set -e
 source ./.env
 docker compose down || :
-docker volume rm "${SSH_VOLUME}" || :
+docker volume rm "${CACHE_VOLUME}" "${SSH_VOLUME}" || :
 docker compose -f ./components/compose.yaml --env-file ./.env build
 docker compose build
 docker compose create
 docker compose start
-docker logs proxy 2> /dev/null
+docker logs proxy 2> /dev/null | sed -n '/^-----/,/^-----/p'
 docker image prune --all -f > /dev/null
 docker attach jumper
