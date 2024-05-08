9e7edc116a07ee14a6e480742a6fc4500d95640e
tiawl <pabtomas@protonmail.com>
2024-05-08 14:05:45 +0200

    [update] 1 container = 1 service

--- wget_me.sh
+++ wget_me.sh
@@ -3,11 +3,18 @@
 set -e
 source ./.env
 docker compose down || :
-docker volume rm "${CACHE_VOLUME}" "${SSH_VOLUME}" || :
-docker compose -f ./components/compose.yaml --env-file ./.env build
+docker volume rm "${COLLECTOR_ETC_CRONTABS_VOLUME}" \
+                 "${COLLECTOR_OPT_DATA_VOLUME}" \
+                 "${COLLECTOR_OPT_SCRIPTS_VOLUME}" \
+                 "${COLLECTOR_VAR_LOG_VOLUME}" \
+                 "${PROXY_FS_VOLUME}" \
+                 "${SSH_VOLUME}" \
+                 || :
+docker compose --file ./components/compose.yaml --env-file ./.env build
 docker compose build
 docker compose create
 docker compose start
+docker volume prune --all --force
 docker logs proxy 2> /dev/null | sed -n '/^-----/,/^-----/p'
-docker image prune --all -f > /dev/null
+docker image prune --all --force > /dev/null
 docker attach jumper
