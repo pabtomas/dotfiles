712f8715d41d725b1139e99dd2b628095e151ce7
tiawl <pabtomas@protonmail.com>
2024-05-11 20:54:26 +0200

    [update] use apk --virtual instead of multistaging when possible

--- wget_me.sh
+++ wget_me.sh
@@ -21,10 +21,10 @@ for template in $(find . -type f -name compose.yaml.in)
 do
   source_env "printf '%s\n' \"$(cat "${template}")\"" > "${template%.*}"
 done
-# TODO: remove docker compose down + docker volume rm
-docker compose down --timeout 0 || :
-source_env_without_docker_host '
-  docker volume rm $(docker volume list --filter "name=${DELETE_ME_SFX}" --format "{{ .Name }}")' || :
+# TODO: manage this differently
+# docker compose down --timeout 0 || :
+# source_env_without_docker_host '
+#   docker volume rm $(docker volume list --filter "name=${DELETE_ME_SFX}" --format "{{ .Name }}")' || :
 docker network prune --force
 docker compose --file ./components/compose.yaml build
 docker compose build
@@ -33,6 +33,6 @@ docker compose start
 docker volume prune --all --force
 source_env_without_docker_host '
   docker logs "${PROXY_ID}" 2> /dev/null | sed -n "/^-----\+$/,/^-----\+$/p"'
-docker image prune --all --force > /dev/null
-docker attach jumper
+#docker image prune --all --force > /dev/null
+#docker attach jumper
 rm -rf "${TRASH_PATH}"
