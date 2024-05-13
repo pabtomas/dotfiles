a4a114b075c4e39a043c27dca0344e0b90d970b2
root <root@explorer.shell>
2024-05-13 15:53:39 +0000

    [update] wget_me.sh: autoupdate/docker install/docker conf + fix pass component

--- wget_me.sh
+++ wget_me.sh
@@ -26,6 +26,17 @@ main ()
 {
   set -eu
 
+  if ! command -v docker; then wget -q -O- https://get.docker.com | sudo sh; fi
+
+  local dist
+  dist="$(. /etc/os-release && printf '%s\n' "${ID}")"
+  readonly dist
+
+  case "${dist}" in
+  ( 'ubuntu'|'debian' ) sudo apt-get update; sudo apt-get upgrade ;;
+  ( * ) printf 'Can not update Docker packages: unknown OS: %s\n' "${dist}" >&2; return 1 ;;
+  esac
+
   local tmp dir_tmp base_tmp
   tmp="$(mktemp --directory)"
   dir_tmp="$(dirname "${tmp}")"
@@ -35,6 +46,20 @@ main ()
   git clone --depth 1 --branch "${2:-trunk}" https://github.com/tiawl/my-whale-fleet.git "${tmp}" || \
   docker run --rm --volume "${HOME}:/root" --volume "${dir_tmp}:/git" 'alpine/git:user' \
     clone --depth 1 --branch "${2:-trunk}" https://github.com/tiawl/my-whale-fleet.git "${base_tmp}"
+
+  if [ ! -e /etc/docker/daemon.json ] || ! diff /etc/docker/daemon.json "${tmp}/host/etc/docker/daemon.json" > /dev/null
+  then
+    cp -f "${tmp}/host/etc/docker/daemon.json" /etc/docker/daemon.json
+    if command -v systemctl
+    then
+      sudo systemctl restart docker
+    else if command -v service
+      sudo service docker restart
+    else
+      printf 'Can not restart Dockerd: unknown service manager\n' >&2
+      return 1
+    fi
+  fi
 
   TRASH_PATH="$(mktemp --directory)"
   API_TAG="$(docker version --format '{{ .Server.APIVersion }}')"
@@ -49,14 +74,15 @@ main ()
 
   docker network prune --force
   docker compose --file "${tmp}/components/compose.yaml" build
-  docker compose --file "${tmp}/compose.yaml" build
-  docker compose --file "${tmp}/compose.yaml" create --no-recreate
-  docker compose --file "${tmp}/compose.yaml" start #--abort-on-container-failure
+  docker compose --file "${tmp}/compose.yaml" up --no-recreate --abort-on-container-failure
   docker volume prune --all --force
   source_env_without_docker_host "${tmp}" \
     'docker logs "${PROXY_ID}" 2> /dev/null | sed -n "/^-----\+$/,/^-----\+$/p"'
   docker image prune --force > /dev/null
   if [ "${1:-}" != '--no-attach' ]; then docker compose --file "${tmp}/compose.yaml" attach jumper; fi
+
+  wget -q -O "$(cd -- "$(dirname -- "${0}")" &> /dev/null && pwd)/$(basename -- "${0}")" \
+    https://raw.githubusercontent.com/tiawl/MyWhaleFleet/trunk/wget_me.sh
 }
 
 main ${@}
