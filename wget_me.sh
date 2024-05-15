c9fb9d009091905ab5b67775a978305b1220ff5f
root <root@explorer.shell>
2024-05-15 21:47:13 +0000

    [fix]

--- wget_me.sh
+++ wget_me.sh
@@ -39,14 +39,13 @@ main ()
 
   source_env ()
   (
-    set -a -- "${1}"
+    set -a
     . "${1}/env.sh"
     eval "${2}"
   )
 
   source_env_without_docker_host ()
   (
-    set -- "${1}"
     . "${1}/env.sh"
     unset DOCKER_HOST
     eval "${2}"
