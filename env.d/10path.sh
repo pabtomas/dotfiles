60bb02b8896ea34787c3cd337e6072686b9171a8
root <root@explorer.shell>
2024-05-15 22:30:11 +0000

    [fix]

--- env.d/10path.sh
+++ env.d/10path.sh
@@ -6,18 +6,18 @@ path 'data' '/opt/data'
 path 'crontabs' '/etc/crontabs'
 path 'crontabs_log' '/var/log/cron.log'
 path 'etc_ngx' '/etc/nginx'
-path 'docker' '/usr/local/bin'
+path "${DOCKER_ID}" '/usr/local/bin'
 path 'opt_scripts' '/opt/scripts'
 path 'opt_ssh' '/opt/ssh'
-path 'safedeposit' '/root/.password-store'
+path "${SAFEDEPOSIT_ID}" '/root/.password-store'
 path 'socket' '/var/run/docker.sock'
 path 'ssh_root' '/root/.ssh'
 path 'tpm' '/root/.tmux/plugins/tpm'
 path 'var_log' '/var/log'
-path 'workspaces' '/workspaces'
+path "${WORKSPACES_ID}" '/workspaces'
 path 'completion' "${DATA_PATH}/99completion"
-path 'entrypoint' "${OPT_SCRIPTS_PATH}/docker_entrypoint.sh"
+path '${ENTRYPOINT_ID}' "${OPT_SCRIPTS_PATH}/docker_entrypoint.sh"
 path 'entrypointd' "$(dirname "${ENTRYPOINT_PATH}")/$(basename -s '.sh' "${ENTRYPOINT_PATH}").d"
 path 'cron_log' "${VAR_LOG_PATH}/cron.log"
-path 'my_whale_fleet' "${WORKSPACES_PATH}/${COMPOSE_PROJECT_NAME}"
-path 'spaceporn' "${WORKSPACES_PATH}/${SPACEPORN_ID}"
+path "${COMPOSE_PROJECT_NAME}" "${WORKSPACES_PATH}/${COMPOSE_PROJECT_NAME}"
+path "${SPACEPORN_ID}" "${WORKSPACES_PATH}/${SPACEPORN_ID}"
