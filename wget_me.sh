1b216c01d94afc799ceb4ace64e62f09a3ad4f71
tiawl <pabtomas@protonmail.com>
2024-05-15 10:48:30 +0200

    [fix]

--- wget_me.sh
+++ wget_me.sh
@@ -104,25 +104,15 @@ main ()
   printf 'Sleeping ...\n'
   sleep 3
 
-  local down_services uncreated_services
-  down_services="$(docker compose --file "${tmp}/compose.yaml" ps \
-    --filter 'status=exited' --filter 'status=restarting' \
-    --filter 'status=removing' --filter 'status=paused' \
-    --filter 'status=dead' --filter 'status=created' --format '{{ .Names }}')"
-  uncreated_services="$(printf '%s\n' \
+  local failed_services
+  failed_services="$(printf '%s\n' \
     $(docker compose --file "${tmp}/compose.yaml" ps --filter 'status=running' --format '{{ .Names }}') \
     $(docker compose --file "${tmp}/compose.yaml" config --services) | sort | uniq -u)"
-  readonly down_services uncreated_services
+  readonly failed_services
 
-  if [ -n "${down_services}" ]
+  if [ -n "${failed_services}" ]
   then
-    printf 'This service did not start correctly: %s\n' ${down_services} >&2
-    return 1
-  fi
-
-  if [ -n "${uncreated_services}" ]
-  then
-    printf 'This service was not created correctly: %s\n' ${uncreated_services} >&2
+    printf 'This service failed: %s\n' ${failed_services} >&2
     return 1
   fi
 
