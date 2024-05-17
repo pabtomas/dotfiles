083fb16a17d9a8c9c4ca8e5d06b8ca389df34551
root <root@explorer.shell>
2024-05-17 11:46:10 +0000

    [fix]

--- wget_me.sh
+++ wget_me.sh
@@ -61,8 +61,11 @@ main ()
     if [ -n "${DEBUG:-}" ]; then set -x; fi
     CDPATH='' cd -- "$(dirname -- "${0}")" > /dev/null 2>&1
     pwd="$(pwd)"
-    wget -q -O "${pwd}/$(basename -- "${0}")" \
-      "https://raw.githubusercontent.com/${1}/wget_me.sh"
+    {
+      wget -q -O "${pwd}/$(basename -- "${0}")" \
+        "https://raw.githubusercontent.com/${1}/wget_me.sh"
+      exit
+    }
   )
 
   # shellcheck disable=2317
