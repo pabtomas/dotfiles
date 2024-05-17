307401c81cc513c2d9644020e8620eefa8512b04
root <root@explorer.shell>
2024-05-17 20:34:59 +0000

    [update] use local images

--- env.d/15img.sh
+++ env.d/15img.sh
@@ -7,7 +7,6 @@ _extern_img "${ALPINE_ID}" 'docker.io' "${ALPINE_ID}" "${ALPINE_TAG}"
 _extern_img "${BASH_ID}" 'docker.io' "${BASH_ID}" "${BASH_TAG}"
 _extern_img "${DOCKER_ID}" 'docker.io' "${DOCKER_ID}" "${DOCKER_TAG}"
 _extern_img "${LINUXSERVER_PROXY_ID}" 'lscr.io/linuxserver' 'socket-proxy' "${LINUXSERVER_PROXY_TAG}"
-OS_IMG="${ALPINE_IMG}"
 OS_LOCAL_IMG="${ALPINE_LOCAL_IMG}"
 
 _intern_img "${COLLECTOR_ID}" "${COLLECTOR_TAG}"
