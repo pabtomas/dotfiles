82c65985e3e99dc4c0791f9fa37792105a421134
root <root@explorer.shell>
2024-05-26 12:08:40 +0000

    [fix]

--- env.d/10volume.sh
+++ env.d/10volume.sh
@@ -16,6 +16,7 @@ _delme_volume "${COLLECTOR_ID}_opt_data" "${COLLECTOR_ID}-opt-data-fs"
 _delme_volume "${COLLECTOR_ID}_opt_scripts" "${COLLECTOR_ID}-opt-scripts-fs"
 _delme_volume 'ssh' 'shared-ssh'
 _delme_volume "${RELAY_ID}_${XSERVER_ID}_socket" "${RELAY_ID}-${XSERVER_ID}-socket"
+_delme_volume "${RELAY_ID}_var_log" "${RELAY_ID}-var-log-fs"
 _delme_volume "${XSERVER_ID}_etc_nginx" "${XSERVER_ID}-etc-nginx-fs"
 _delme_volume "${XSERVER_ID}_opt_scripts" "${XSERVER_ID}-opt-scripts-fs"
 _delme_volume "${XSERVER_ID}_var_log_nginx" "${XSERVER_ID}-var-log-nginx-fs"
