b70b7f0b65df01c899e945ea0e1be1653b21a38a
root <root@explorer.shell>
2024-05-31 13:53:14 +0000

    [update]

--- env.d/10model.sh
+++ env.d/10model.sh
@@ -3,4 +3,10 @@
 # SC2034: VAR appears unused => VAR used for templating
 # SC2154: VAR is referenced but not assigned => VAR is assigned with eval statement in 01init.sh function
 
+_model "${_DOCKER_ID}"
+_model "${MAN_ID}"
+_model "${PASS_ID}"
+_model "${VIM_ID}"
+_model "${SHELL_ID}"
 _model "${SSHD_ID}"
+_model "${ZID_ID}"
