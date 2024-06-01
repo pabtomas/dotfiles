da57af681fe9ac328df6c73cc735b923e191c713
root <root@explorer.shell>
2024-06-01 21:01:16 +0000

    [fix]

--- env.d/10volume.sh
+++ env.d/10volume.sh
@@ -8,16 +8,17 @@ _volume "${REGISTRY_ID}"
 _volume "${SPACEPORN_ID}"
 _volume "${SAFEDEPOSIT_ID}"
 
-_delme_volume "${PROXY_ID}_etc_nginx" "${PROXY_ID}-etc-nginx-fs"
-_delme_volume "${PROXY_ID}_opt_scripts" "${PROXY_ID}-opt-scripts-fs"
-_delme_volume "${PROXY_ID}_var_log_nginx" "${PROXY_ID}-var-log-nginx-fs"
 _delme_volume "${LISTENER_ID}_opt_data" "${LISTENER_ID}-opt-data-fs"
 _delme_volume "${LISTENER_ID}_opt_scripts" "${LISTENER_ID}-opt-scripts-fs"
 _delme_volume "${LISTENER_ID}_var_log" "${LISTENER_ID}-var-log-fs"
-_delme_volume 'ssh' 'shared-ssh'
+_delme_volume "${PROXY_ID}_etc_nginx" "${PROXY_ID}-etc-nginx-fs"
+_delme_volume "${PROXY_ID}_opt_scripts" "${PROXY_ID}-opt-scripts-fs"
+_delme_volume "${PROXY_ID}_var_log_nginx" "${PROXY_ID}-var-log-nginx-fs"
+_delme_volume "${REGISTRY_ID}_var_log" "${REGISTRY_ID}-var-log-fs"
 _delme_volume "${RELAY_ID}_${XSERVER_ID}_socket" "${RELAY_ID}-${XSERVER_ID}-socket"
 _delme_volume "${RELAY_ID}_var_log" "${RELAY_ID}-var-log-fs"
+_delme_volume 'ssh' 'shared-ssh'
+_delme_volume 'theme' 'theme'
 _delme_volume "${XSERVER_ID}_etc_nginx" "${XSERVER_ID}-etc-nginx-fs"
 _delme_volume "${XSERVER_ID}_opt_scripts" "${XSERVER_ID}-opt-scripts-fs"
 _delme_volume "${XSERVER_ID}_var_log_nginx" "${XSERVER_ID}-var-log-nginx-fs"
-_delme_volume 'theme' 'theme'
