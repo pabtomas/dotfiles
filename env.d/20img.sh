e770118d5c82d3a707c1bb87a00b1ce927529cf1
root <root@explorer.shell>
2024-06-13 14:59:38 +0000

    [update] in progress

--- env.d/20img.sh
+++
@@ -1,7 +0,0 @@
-#! /bin/sh
-# shellcheck disable=2034,2154
-# SC2034: VAR appears unused => VAR used for templating
-# SC2154: VAR is referenced but not assigned => VAR is assigned with eval statement in 01init.sh function
-
-OS_IMG="${ALPINE_IMG}"
-OS_LOCAL_IMG="${ALPINE_LOCAL_IMG}"
